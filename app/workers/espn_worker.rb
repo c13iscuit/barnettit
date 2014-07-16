5b137a02c96f732a6874d54c26461b96bd11b26e
c13iscuit <barnett.chri@gmail.com>
2014-07-16 11:50:25 -0400

    Turn on sidekiq workers in production

--- app/workers/espn_worker.rb
+++ app/workers/espn_worker.rb
@@ -1,19 +1,19 @@
-# class EspnWorker
-#   include Sidekiq::Worker
-#   include Sidetiq::Schedulable
+class EspnWorker
+  include Sidekiq::Worker
+  include Sidetiq::Schedulable
 
-#   recurrence do
-#     hourly(4)
-#   end
+  recurrence do
+    hourly(4)
+  end
 
-#   def perform
-#     uri = URI("http://api.espn.com/v1/sports/news/headlines/top?apikey=#{ESPN_KEY}")
-#     response = Net::HTTP.get(uri)
-#     result = JSON.parse(response)
-#     top = result["headlines"].take(3)
-#     top.each do |article|
-#       post = Post.new(title: article["headline"], url: article["links"]["web"]["href"], user_id: 8)
-#       post.save
-#     end
-#   end
-# end
+  def perform
+    uri = URI("http://api.espn.com/v1/sports/news/headlines/top?apikey=#{ESPN_KEY}")
+    response = Net::HTTP.get(uri)
+    result = JSON.parse(response)
+    top = result["headlines"].take(3)
+    top.each do |article|
+      post = Post.new(title: article["headline"], url: article["links"]["web"]["href"], user_id: 8)
+      post.save
+    end
+  end
+end
